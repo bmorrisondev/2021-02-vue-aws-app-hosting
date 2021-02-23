4efbbd0045dc042e35741b175a787da518a5ec0f
Brian Morrison II <brian@brianmorrison.me>
2021-02-23 11:37:46 -0600

    Updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="GitHub Actions are awesome!"/>
   </div>
 </template>
 
